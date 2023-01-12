757ab947d7e824f1a51c34cc379c50155345d953
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2023-01-11 21:16:02 -0800

    remove some cache stuff

--- src/index.ts
+++ src/index.ts
@@ -21,9 +21,7 @@ export default {
         const matched = await caches.default.match(req);
         if (matched) {
             console.log(`cache found for ${req.url}`);
-            const res = new Response(matched.body, matched);
-            res.headers.set("X-Using-Cache", "true"); // mainly to ensure that the cache is working, might remove later
-            return res;
+            return matched;
         }
 
         console.log(`no cache for ${req.url}`);
