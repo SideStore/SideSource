78a3509762587076232f08c5c7b90ab596715f8f
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2023-02-05 17:29:54 -0800

    fix chalk usage in worker

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,7 @@
+import chalk from "chalk";
+// In a cloudflare worker environment, chalk will think colors aren't supported, so we must force it to allow for basic formatting
+// If you know a way to detect cloudflare workers so we don't enable colors in terminals that don't support them, please open a issue/PR.
+if (chalk.level <= 0) chalk.level = 1;
+
 export * from "#/worker";
 export * from "#/source";
